9b794eccec92489458000e4bbafdfa96879ec05d
Adaikal Raj <adaikal.raj@gmail.com>
2016-02-25 16:16:03 +0530

    Update install_docker_compose.sh

--- install_docker_compose.sh
+++ install_docker_compose.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 install_docker() {
-  apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-  echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
-  apt update
-  apt purge lxc-docker*
-  apt install -y docker-engine
+  sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+  sudo echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
+  sudo apt update
+  sudoapt purge lxc-docker*
+  sudo apt install -y docker-engine
 }
 
 install_compose() {
-  curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-  chmod +x /usr/local/bin/docker-compose
+  sudo curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
 }
 
 which docker >/dev/null
